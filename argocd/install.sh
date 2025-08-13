09cf217fcd7cf8ff13f9c4eecf05582130cbf2a7
linux <larensolarsuss@gmail.com>
2025-08-13 12:37:51 +0200

    fixes

--- argocd/install.sh
+++ argocd/install.sh
@@ -1,1 +1,1 @@
-helm install argo argo/argo-cd --version 8.2.7 -f vals.yml
+helm install argocd argo/argo-cd --version 8.2.7 -f vals.yml
---
+++ k3s/upgrade.sh
@@ -0,0 +1,1 @@
+curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=v1.32.7+k3s1 sh -s
